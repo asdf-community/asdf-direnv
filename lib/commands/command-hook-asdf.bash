ef5d6dd27f49868d40fea1d93f6dd0e0d88f9b50
Victor Borja <vborja@apache.org>
2022-04-03 09:16:34 -0500

    Update lib/commands/command-hook-asdf.bash
    Co-authored-by: Jeremy Fleischman <jeremyfleischman@gmail.com>

--- lib/commands/command-hook-asdf.bash
+++ lib/commands/command-hook-asdf.bash
@@ -6,6 +6,6 @@ Looks like you are using the 'asdf direnv hook asdf' command which was removed i
 
 Remove the line that reads 'asdf direnv hook asdf' from your rc-file: ~/.config/direnv/direnvrc
 
-And run `asdf direnv setup --help` for using the new setup.
+And run `asdf direnv setup --help` for instructions to perform.
 EOF
 exit 1
