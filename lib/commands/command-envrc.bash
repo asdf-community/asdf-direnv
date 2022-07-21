875d3a6208c1f6fbd58487c900c828dde7854997
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-07-21 14:36:56 -0700

    Remove error trap
    This wasn't doing what I wanted it to: I don't fully understand why =(

--- lib/commands/command-envrc.bash
+++ lib/commands/command-envrc.bash
@@ -5,9 +5,4 @@ set -Eeuo pipefail
 # shellcheck source=lib/tools-environment-lib.bash
 source "$(dirname "${BASH_SOURCE[0]}")/../tools-environment-lib.bash"
 
-function print_generic_error() {
-  log_error "Error generating asdf cached envrc"
-}
-trap print_generic_error ERR
-
 _print_asdf_cached_envrc "$@"
